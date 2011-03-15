72dd76281812289ac0e8a301625c3f19d0b5f929
rmacfie <robert@macfie.se>
2011-03-15 20:01:13 +0100

    Formatting

--- MinimumWCF.Client/Program.cs
+++ MinimumWCF.Client/Program.cs
@@ -8,21 +8,20 @@ namespace MinimumWCF.Client
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to the Calculator client.");
-
 			var sum = 0;
 
 			while (true)
 			{
 				Console.WriteLine("Current sum: {0}. Please type a new number and press <enter> to calculate.", sum);
-
 				var input = int.Parse(Console.ReadLine());
-
 				Console.Write("Calculating {0} + {1}", sum, input);
+
 
 				using (var calculatorProxy = new CalculatorProxy("http://localhost:12345/"))
 				{
 					sum = calculatorProxy.Add(input, sum);
 				}
+
 
 				Console.WriteLine(" = {0}", sum);
 			}
